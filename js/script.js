547d5f3f87d7fd0df62bcf6bbe84350ebbe52574
SirBoesly <sirboesly@gmail.com>
2024-04-18 22:50:20 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika hamburger diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
